e6510edb8b1c5ce640ae8f55212737cc9b838746
anshika056 <anshikamadan2@gmail.com>
2022-03-13 00:00:30 +0530

    setup of html

---
+++ test/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express()
+
+const http = require("http").Server(app);
+const port = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+http.listen(port, function() {
+    console.log("Listening on :" + port);
+});
